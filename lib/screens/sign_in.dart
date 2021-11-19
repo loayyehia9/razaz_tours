e8fd59cb8fc5f2e785c2ecba265eb56fee0d6330
Loay Yehia <75745952+loayyehia9@users.noreply.github.com>
2021-11-20 00:12:57 +0200

    create sign in.dart

---
+++ lib/screens/sign_in.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
