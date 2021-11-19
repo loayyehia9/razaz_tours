95cd3da7d21c094c5bce0d63cf5b4f21ecf599c4
Loay Yehia <75745952+loayyehia9@users.noreply.github.com>
2021-11-20 00:50:24 +0200

    add photo and main done

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:razaz_tours/screens/sign_in.dart';
 
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Rzaz Tours',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SignIn(),
+      },
+    );
+  }
+}
